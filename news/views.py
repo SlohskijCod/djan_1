ccb66e5ab47feaef6cceea47f15dbdfb1473b4f8
ADMIN <slim080885@gmail.com>
2022-06-23 16:03:17 +0300

    new html

--- news/views.py
+++ news/views.py
@@ -4,4 +4,4 @@ from .models import Articles
 
 def news_home(request):
     news = Articles.objects.all()
-    return render(request, 'news/news_home.html')
+    return render(request, 'news/news_home.html', {'news': news})
